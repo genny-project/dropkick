b849c54b3c6b595829a8928e3870d9b5036d7785
Adam Crow <adamcrow63@gmail.com>
2021-10-24 01:10:17 +1100

    Native running well

--- run-native.sh
+++ run-native.sh
@@ -1,5 +1,8 @@
 #!/bin/bash
-
+export GENNY_SHOW_VALUES=TRUE
+export GENNY_SERVICE_USERNAME=service
+export GENNY_KEYCLOAK_URL=https://keycloak-office.gada.io
+export GENNY_API_URL=http://internmatch.genny.life:8280
 echo $GENNY_KEYCLOAK_URL
 echo $GENNY_SERVICE_USERNAME
 echo $GENNY_SERVICE_PASSWORD
@@ -7,4 +10,4 @@ echo $GENNY_CLIENT_ID
 echo $GENNY_CLIENT_SECRET
 echo $GENNY_REALM
 
-export GENNY_SHOW_VALUES=true; ./target/lauchy-9.8.0-runner
+export GENNY_SHOW_VALUES=true; ./target/dropkick-9.9.0-runner
